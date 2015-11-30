3b054af9c87dc2e0e54bff2347d9912f8ec7686b
Keheira <keheirah@gmail.com>
2015-11-29 20:31:16 -0500

    Took out css in index

--- learn.js
+++ learn.js
@@ -2,25 +2,30 @@ $(document).ready(function(){
     // Show the first tab.
     $("#video").show();
     $("#practice").hide();
-}
-
-var coordA = [document.getElementById('ax'), document.getElementById('ay')];
-var coordB = [document.getElementById('bx'), document.getElementById('by')];
-var coordC = [document.getElementById('cx'), document.getElementById('cy')];
-
-coordA[0].value = rng(0,10);
-coordA[1].value = rng(0,10);
-coordB[0].value = rng(0,10);
-coordB[1].value = rng(0,10);
-coordC[0].value = rng(0,10);
-coordC[1].value = rng(0,10);
-
-function onNext(){
     
-}
+    $(".next").click(function(){
+        $("#video").hide();
+        $("#practice").css('top', h/2-$("#signin").height()/2);
+        $("#practice").css('left', w/2-$("#signin").width()/2);
+        $("#practice").show();
+        var coordA = [document.getElementById('ax'), document.getElementById('ay')];
+        var coordB = [document.getElementById('bx'), document.getElementById('by')];
+        var coordC = [document.getElementById('cx'), document.getElementById('cy')];
 
-function onPrev(){
+        coordA[0].value = rng(0,10);
+        coordA[1].value = rng(0,10);
+        coordB[0].value = rng(0,10);
+        coordB[1].value = rng(0,10);
+        coordC[0].value = rng(0,10);
+        coordC[1].value = rng(0,10);
+    })
     
+    $(".prev").click(function(){
+        $("#video").show();
+        $("#video").css('top', h/2-$("#signin").height()/2);
+        $("#video").css('left', w/2-$("#signin").width()/2);
+        $("#practice").hide();
+    })
 }
 
 function rng(min,max){
